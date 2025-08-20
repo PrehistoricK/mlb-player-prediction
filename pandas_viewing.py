e9a048ca7156d9d15eb00272562d9016a7710719
Prakash Kumar <p6kumar@uwaterloo.ca>
2025-08-20 19:56:00 -0400

    playing aorund with dataset using pandas

---
+++ pandas_viewing.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+mlb = pd.read_csv("batting.csv")
+mlb.dropna()
+#print(mlb.shape)
+mlb.drop(["AVG", "OBP", "SLG"], axis=1,inplace=True)
+#print(mlb.shape)
+
+mlb["AVG"] = mlb["H"]/mlb["AB"]
+mlb["OBP"] = (mlb["H"]+mlb["BB"]+mlb["HBP"])/(mlb["AB"]+mlb["BB"]+mlb["HBP"]+mlb["SF"])
+mlb["SLG"] = (mlb["1B"]+2*mlb["2B"]+3*mlb["3B"]+4*mlb["HR"])/mlb["AB"]
+
+#print(mlb.shape)
+
+top_avg = mlb.sort_values("AVG",ascending=False).head(10)
+top_obp = mlb.sort_values("OBP",ascending=False).head(10)
+top_slg = mlb.sort_values("SLG",ascending=False).head(10)
+
+print(top_avg[["Name","AVG"]])
+print(top_obp[["Name","OBP"]])
+print(top_slg[["Name","SLG"]])
